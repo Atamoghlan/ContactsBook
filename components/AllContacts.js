8a086f5e326d643393b3171461f1ee429d5b7d91
atamoghlan <atamoghlan.a.aliyev@gmail.com>
2020-12-27 22:41:33 +0400

    28

--- components/AllContacts.js
+++ components/AllContacts.js
@@ -10,11 +10,11 @@ const mapStateToProps = state => {
 class AllContacts extends Component {
   render() {
     return(
-      <View style={{marginTop: 15, marginLeft: 10,flex:1}}>
+      <View style={styles.container}>
         {this.props.usersList?.map(item => {
           return (
-          <View key={item.id} style={{flex:0.5}}>
-            <Text style={{marginLeft: 10,fontSize: 30}}>{item.name}:{item.phoneNumber}</Text>
+          <View key={item.id} style={styles.viewOfText}>
+            <Text style={styles.text}>{item.name}:{item.phoneNumber}</Text>
           </View>
         )})}
       </View>
@@ -23,3 +23,15 @@ class AllContacts extends Component {
 }
 
 export default connect(mapStateToProps)(AllContacts);
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 15, marginLeft: 10,flex:1
+},
+text: {
+  marginLeft: 10,fontSize: 30
+},
+viewOfText: {
+  flex:0.5
+}
+})
